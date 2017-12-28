ca6a125fd3cc435a8a6ef0e4fcc619706259bd7d
David Garcia <dgarcia@teravisiontech.com>
2017-12-28 16:05:10 -0400

    Agregadas las fuentes

--- Postcards From Paradise/Controllers/ViewController.swift
+++ Postcards From Paradise/Controllers/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController, UICollectionViewDataSource, UITableViewD
     @IBOutlet weak var postcardImageView: UIImageView!
     @IBOutlet weak var colorCollectionView: UICollectionView!
     
+    var colors = [UIColor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.colors += [.black, .blue, .green, .yellow, .white, .gray, .red, .orange, .cyan, .purple, .magenta]
+        for hue in 0...9 {
+            for sat in 1...10 {
+                let color = UIColor(hue: CGFloat(hue)/10, saturation: CGFloat(sat)/10, brightness: 1, alpha: 1)
+                self.colors.append(color)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,12 +31,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, UITableViewD
         // Dispose of any resources that can be recreated.
     }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //MARK: Collection View Data Source
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.colors.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+        let color = self.colors[indexPath.row]
+        cell.backgroundColor = color
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 5
+        return cell
     }
 }
 
