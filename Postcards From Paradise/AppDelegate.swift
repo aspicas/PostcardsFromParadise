3e746faea8e5a0310fe67b6292c6fb587a3fd965
David Garcia <dgarcia@teravisiontech.com>
2017-12-29 16:23:19 -0400

    left buttom in the second view controller

--- Postcards From Paradise/AppDelegate.swift
+++ Postcards From Paradise/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let splitView =  window?.rootViewController as? UISplitViewController {
+            splitView.preferredDisplayMode = .allVisible
+            if let navController = splitView.viewControllers.last as? UINavigationController {
+                navController.topViewController?.navigationItem.leftBarButtonItem = splitView.displayModeButtonItem
+            }
+        }
         return true
     }
 
