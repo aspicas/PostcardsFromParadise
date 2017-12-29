1ddbc855cc14ff20710966447a4c12f463e1d829
David Garcia <dgarcia@teravisiontech.com>
2017-12-29 14:21:09 -0400

    Drag

--- Postcards From Paradise/Controllers/ViewController.swift
+++ Postcards From Paradise/Controllers/ViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UITableViewDelegate {
-
+class ViewController: UIViewController,
+                    UICollectionViewDataSource,
+                    UICollectionViewDelegate,
+                    UICollectionViewDragDelegate,
+UICollectionViewDropDelegate{
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        <#code#>
+    }
+    
+    
     @IBOutlet weak var postcardImageView: UIImageView!
     @IBOutlet weak var colorCollectionView: UICollectionView!
     
@@ -35,6 +43,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UITableViewD
                 self.colors.append(color)
             }
         }
+        
+        self.colorCollectionView.dragDelegate = self
+        
+        self.postcardImageView.isUserInteractionEnabled = true
+        let dropInteraction = UIDropInteraction(delegate: self as! UIDropInteractionDelegate)
+        self.postcardImageView.addInteraction(dropInteraction)
+        
+        renderPostcard()
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,23 +78,59 @@ class ViewController: UIViewController, UICollectionViewDataSource, UITableViewD
     
     func renderPostcard(){
         // 1. Definir la zona de dibujo rectangular para trabajar. 3000x2400
+        let drawRect = CGRect(x: 0, y: 0, width:  3000, height: 2400)
         
         // 2. Crear dos rectangulos para los dos textos de la postal
+        let topRect = CGRect(x: 300, y: 200, width: 2400, height: 800)
+        let bottomRect = CGRect(x: 300, y: 1800, width: 2400, height: 600)
         
         // 3. A partir de los nombres de las fuentes, crear los objetos UIFont
-        // Dejaremos una fuente por defecto (Avenir Next) por si algo falla
+        // Dejaremos una fuente por defecto (La de sistema) por si algo falla
+        let topFont = UIFont(name: self.topFontName, size: 250) ?? UIFont.systemFont(ofSize: 240)
+        let bottomFont = UIFont(name: self.bottomFontName, size: 120) ?? UIFont.systemFont(ofSize: 80)
         
         // 4. NSMutableParagraphStyle para centrar el texto en la etiqueta
+        let centered = NSMutableParagraphStyle()
+        centered.alignment = .center
         
         // 5. Definir la estructura de la etiqueta como el color y la fuente. (attributed strings (NSAttributedStringKey))
+        let topAttributes : [NSAttributedStringKey: Any] =
+            [
+                .foregroundColor : topFontColor,
+                .font : topFont,
+                .paragraphStyle : centered
+            ]
+        
+        let bottomAttributes : [NSAttributedStringKey: Any] =
+            [
+                .foregroundColor : bottomFontColor,
+                .font : bottomFont,
+                .paragraphStyle : centered
+            ]
         
         // 6. Iniciar la renderizacion de la imagen (UIGraphicsImageRenderer)
-        
-            // 6.1 Renderizar la zona con un fondo gris
-        
-            // 6.2 Pintaremos la imagen seleccionada del usuario empezando por el borde superior izquierdo.
-        
-            // 6.3 Pintar las dos etiquetas de texto con los parámetros configurados en 5
+        let renderer = UIGraphicsImageRenderer(size: drawRect.size)
+        self.postcardImageView.image = renderer.image(actions:
+            { (context) in
+                // 6.1 Renderizar la zona con un fondo gris
+                UIColor.lightGray.set()
+                context.fill(drawRect)
+                
+                // 6.2 Pintaremos la imagen seleccionada del usuario empezando por el borde superior izquierdo.
+                self.image?.draw(at: CGPoint(x: 0, y: 0))
+                
+                // 6.3 Pintar las dos etiquetas de texto con los parámetros configurados en 5
+                self.topText.draw(in: topRect, withAttributes: topAttributes)
+                self.bottomText.draw(in: bottomRect, withAttributes: bottomAttributes)
+            })
+    }
+    
+    // MARK: UICollectionViewDragDelegate
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let color = self.colors[indexPath.row]
+        let itemProvider = NSItemProvider(object: color)
+        let item = UIDragItem(itemProvider: itemProvider)
+        return [item]
     }
 }
 
